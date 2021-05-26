27f649f68bb3318424ae6fe1ff508588b8e34520
ayishafida <79889370+ayishafida@users.noreply.github.com>
2021-05-26 12:03:39 +0530

    Add files via upload

---
+++ 1-15/pg3.java
@@ -0,0 +1,13 @@
+//print result of operations
+class pg3{
+	public static void main(String args[]){
+		int a=-5+8*6;
+		int b=(55+9)%9;
+		int c=20+-3*5/8;
+		int d=5+15/3*2-8%3;
+		System.out.println("-5+8*6="+a);
+    		System.out.println("(55+9)%9="+b);
+		System.out.println("20+-3*5/8="+c);
+		System.out.println("5+15/3*2-8%3="+d);
+	}
+}
---
+++ 1-15/pg4.java
@@ -0,0 +1,10 @@
+//area and perimeter of cirlce
+class pg4{
+	public static void main(String args[]){
+		double r=7.5;
+		double a=3.14*r*r;
+		double p=2*3.14*r;
+		System.out.println("Perimeter is = "+p);
+		System.out.println("Area is = "+a);
+	}
+}
